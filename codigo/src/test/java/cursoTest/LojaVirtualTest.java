712d9c5862d44657350e2bae6c7224068249b1bb
Gustavo Dias <gustavodias2@gmail.com>
2020-08-26 20:47:32 -0300

    Aula4: Xpath, CssSelector e refatoração

--- codigo/src/test/java/cursoTest/LojaVirtualTest.java
+++ codigo/src/test/java/cursoTest/LojaVirtualTest.java
@@ -1,42 +1,37 @@
 package cursoTest;
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
 
-public class LojaVirtualTest {
-
-	WebDriver driver;
-
-	public void abreNavegador() {
-		System.setProperty("webdriver.chrome.driver", "driver/chromedriver.exe");
-		driver = new ChromeDriver();
-		driver.get("https://lojaexemplod.lojablindada.com");
-		driver.manage().window().maximize();
-	}
-
-	public void fechaNavegador() {
-		driver.quit();
-	}
+public class LojaVirtualTest extends BaseTest {
 
 	@Test
-	public void abreFechaNavegador() {
-		abreNavegador();
-		fechaNavegador();
-	}
-
-	@Test
-	public void pesquisaLivro() {
-		abreNavegador();
-
+	public void pesquisaLivro() throws InterruptedException {
 		WebElement pesquisa = driver.findElement(By.id("search"));
-		pesquisa.sendKeys("fortaleza digital");
-		pesquisa.sendKeys(Keys.ENTER);
+		// Opção 1
+		// pesquisa.sendKeys("fortaleza digital");
+		// pesquisa.sendKeys(Keys.ENTER);
 
-		fechaNavegador();
+		// Opção 2
+		pesquisa.sendKeys("fortaleza digital", Keys.ENTER);
+
+		WebElement elTituloLivro = driver.findElement(By.xpath("//h2/a"));
+		String livro = elTituloLivro.getText();
+
+		Assert.assertEquals("[PRODUTO DE EXEMPLO] - Fortaleza Digital", livro);
+		Thread.currentThread().sleep(5000);
+
+		// #   -> id
+		// .   -> class
+		// ' ' -> tag
+
+		WebElement elPreco = driver.findElement(By.cssSelector("#product-price-44 > span"));
+		String preco = elPreco.getText();
+		Assert.assertEquals("R$519,90", elPreco);
+
 	}
 
 }
