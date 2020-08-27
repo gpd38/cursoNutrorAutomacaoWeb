9b169b88a29bd075b39f095075c603dc2ede1fd9
Gustavo Dias <gustavodias2@gmail.com>
2020-08-26 21:33:43 -0300

    Aula5: AssertThat e ListWebElements

--- codigo/src/test/java/cursoTest/LojaVirtualTest.java
+++ codigo/src/test/java/cursoTest/LojaVirtualTest.java
@@ -1,5 +1,10 @@
 package cursoTest;
 
+import static org.hamcrest.CoreMatchers.*;
+import static org.hamcrest.MatcherAssert.*;
+
+import java.util.List;
+
 import org.junit.Assert;
 import org.junit.Test;
 import org.openqa.selenium.By;
@@ -24,8 +29,8 @@ public class LojaVirtualTest extends BaseTest {
 		Assert.assertEquals("[PRODUTO DE EXEMPLO] - Fortaleza Digital", livro);
 		Thread.currentThread().sleep(5000);
 
-		// #   -> id
-		// .   -> class
+		// # -> id
+		// . -> class
 		// ' ' -> tag
 
 		WebElement elPreco = driver.findElement(By.cssSelector("#product-price-44 > span"));
@@ -34,4 +39,40 @@ public class LojaVirtualTest extends BaseTest {
 
 	}
 
+	@Test
+	public void pesquisaLivroAssertThat() {
+		WebElement pesquisa = driver.findElement(By.id("search"));
+		pesquisa.sendKeys("fortaleza digital", Keys.ENTER);
+
+		WebElement elTituloLivro = driver.findElement(By.xpath("//h2/a"));
+		String livro = elTituloLivro.getText();
+
+		Assert.assertEquals("[PRODUTO DE EXEMPLO] - Fortaleza Digital", livro);
+
+		WebElement elPreco = driver.findElement(By.cssSelector("#product-price-44 > span"));
+		String preco = elPreco.getText();
+
+		assertThat("R$519,90", is(preco));
+		assertThat("R$519,908", containsString(preco));
+	}
+
+	@Test
+	public void testClickLista() {
+		WebElement pesquisa = driver.findElement(By.id("search"));
+		pesquisa.sendKeys("fortaleza digital", Keys.ENTER);
+
+		List<WebElement> elLivros = driver.findElements(By.cssSelector("ul.products-grid > li"));
+		WebElement elTituloLivro, elPreco;
+		String tituloLivro;
+		for (WebElement elLivro : elLivros) {
+			elTituloLivro = elLivro.findElement(By.cssSelector("h2 > a"));
+			tituloLivro = elTituloLivro.getText();
+			if (tituloLivro.contains("Ajax com Java")) {
+				elPreco = elLivro.findElement(By.cssSelector("span.price"));
+				assertThat("R$444,50", is(elPreco.getText()));
+				break;
+			}
+		}
+
+	}
 }
