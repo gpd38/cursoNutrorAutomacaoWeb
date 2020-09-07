2d8a1ebe6665461d8d42af7cb6915d0fbf3e16b0
Gustavo Dias <gustavodias2@gmail.com>
2020-09-06 23:34:09 -0300

    iniciando alterações

--- codigo/src/test/java/cursoTest/BaseTest.java
+++ codigo/src/test/java/cursoTest/BaseTest.java
@@ -2,8 +2,6 @@ package cursoTest;
 
 import org.junit.After;
 import org.junit.Before;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 
 import core.Driver;
 
--- codigo/src/test/java/pages/InicialPage.java
+++ codigo/src/test/java/pages/InicialPage.java
@@ -1,5 +1,7 @@
 package pages;
 
+import java.util.List;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 
@@ -9,7 +11,10 @@ public class InicialPage {
 
 	String elElemento = "search";
 	String elTituloLivro = "//h2/a";
+	String elTituloLivroLista = "h2 > a";
 	String elPreco = "#product-price-44 > span";
+	String elLivros = "ul.products-grid > li";
+	String elPrecoLista = "span.price";
 
 	/**
 	 * @param valor
@@ -26,11 +31,24 @@ public class InicialPage {
 		String livro = tituloLivro.getText();
 		return livro;
 	}
-	
+
 	public String getPreco() {
 		WebElement preco = Driver.getDriver().findElement(By.cssSelector(this.elPreco));
-		String valor = preco.getText();	
+		String valor = preco.getText();
 		return valor;
+	}
+
+	public String getPrecoLista() {
+		List<WebElement> listaLivros = Driver.getDriver().findElements(By.cssSelector(elLivros));
+		for (WebElement elLivro : listaLivros) {
+			WebElement tituloLivro = elLivro.findElement(By.xpath(elTituloLivroLista));
+			String tituloLivrounitario = tituloLivro.getText();
+			if (tituloLivrounitario.contains("Ajax com Java")) {
+				WebElement preco = elLivro.findElement(By.cssSelector(elPrecoLista));
+				return preco.getText();
+			}
+		}
+		return null;
 	}
 
 }
